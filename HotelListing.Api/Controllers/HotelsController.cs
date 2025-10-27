17a582d0796abc42c6e998de5c1281f89fb79638
Mousa Shawar <mshawar.dev@outlook.com>
2025-10-28 01:27:11 +0300

    Add project files.

---
+++ HotelListing.Api/Controllers/HotelsController.cs
@@ -0,0 +1,88 @@
+﻿using HotelListing.Api.Data;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HotelListing.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelsController : ControllerBase
+    {
+        private static List<Hotel> Hotels = new List<Hotel>
+         {
+             new Hotel { HotelId = 1, Name = "Hotel One", Address = "123 Main St", PhoneNumber = "555-1234", Rating = 4.7 },
+             new Hotel { HotelId = 2, Name = "Hotel Two", Address = "456 Elm St", PhoneNumber = "555-5678", Rating = 2.5 },
+             new Hotel { HotelId = 3, Name = "Hotel Three", Address = "789 Oak St", PhoneNumber = "555-9012", Rating = 3.4 }
+         };
+
+        // GET: api/<HotelsController>
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return Ok(Hotels);
+        }
+
+        // GET api/<HotelsController>/5
+        [HttpGet("{id}")]
+        public ActionResult<Hotel> Get(int id)
+        {
+            var hotel = Hotels.FirstOrDefault(h => h.HotelId == id);
+            if (hotel is null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(hotel);
+            }
+        }
+
+        // POST api/<HotelsController>
+        [HttpPost]
+        public ActionResult<Hotel> Post([FromBody] Hotel newHotel)
+        {
+            if (Hotels.Any(h => h.HotelId == newHotel.HotelId))
+            {
+                return BadRequest("Already exist");
+            }
+
+            Hotels.Add(newHotel);
+
+            return CreatedAtAction(nameof(Get), new { id = newHotel.HotelId }, newHotel);
+        }
+
+        // PUT api/<HotelsController>/5
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] Hotel updatedHotel)
+        {
+            var existingHotel = Hotels.FirstOrDefault(h => h.HotelId == id);
+            if (existingHotel is null)
+            {
+                return NotFound();
+            }
+
+            existingHotel.Name = updatedHotel.Name;
+            existingHotel.Address = updatedHotel.Address;
+            existingHotel.PhoneNumber = updatedHotel.PhoneNumber;
+            existingHotel.Rating = updatedHotel.Rating;
+
+            return NoContent();
+        }
+
+        // DELETE api/<HotelsController>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var hotel = Hotels.FirstOrDefault(h => h.HotelId == id);
+            if (hotel is null)
+            {
+                return NotFound(new { message = "Hotel not found"});
+            }
+
+            Hotels.Remove(hotel);
+
+            return NoContent();
+        }
+    }
+}
---
+++ HotelListing.Api/Data/Hotel.cs
@@ -0,0 +1,11 @@
+﻿namespace HotelListing.Api.Data
+{
+    public class Hotel
+    {
+        public int HotelId { get; set; }
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+        public string? PhoneNumber { get; set; }
+        public double Rating { get; set; }
+    }
+}
